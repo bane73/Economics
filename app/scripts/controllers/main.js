fcddd80bde873ea4fc3de4a7a110ba46f74f6601
TC <thoskr@gmail.com>
2013-01-03 20:16:53 +0100

    Economics operational
    Read and write data from/to Firebase working.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,63 @@
 'use strict';
 
-EconomicsApp.controller('MainCtrl', function($scope, firebase) {
-  //$scope.lines = firebase.get;
+EconomicsApp.controller('MainCtrl', function($scope)
+{
+	// $scope.lines = firebase.$get();
 
-  $scope.lines = [{name:"Kenneth", date:"24-12-2012", amount:"200"}, {name:"Are", date:"28-12-2012", amount:"300"}, {name:"Erich", date:"30-12-2012", amount:"400"}, {name:"TC", date:"01-01-2013", amount:"500"}];
+	// console.log(firebase.$get());
 
+	$scope.$apply(function()
+	{
+		$scope.users = ["Are", "Erich", "Kenneth", "TC"];
+	});
 
+	$scope.lines = "";
+
+	
+		var db = new Firebase('https://creative.firebaseio.com/stp_okonomi');
+
+	    db.on('value', function(data)
+	    {
+	    	$scope.$apply(function()
+			{
+	    		// console.log(_.toArray(data.val()).slice(1));
+	        	$scope.lines = _.toArray(data.val()).slice(1);
+	        });
+	    });
+	
+
+	$scope.addLine = function()
+    {
+        var now;
+
+        if($scope.Date == null || $scope.Date == "")
+        {
+            var d = new Date();
+            now = d.getFullYear()+ '-' +((d.getMonth()+1)) + '-' + d.getDate();
+            $scope.Date = now;
+        }
+
+        else
+        {
+            now = $scope.Date;
+        }
+        
+        var obj = {"date":now, "name":$scope.Name, "amount":$scope.Amount};
+
+        db.push(obj);
+
+        // $scope.lines.push(obj);
+        $scope.StpAmount = '';
+    };
+
+	$scope.total = function()
+    {
+        var total = 0;
+        angular.forEach($scope.lines, function(line)
+        {
+            total += line.amount;
+        });
+
+        return total;
+    };
 });
--- app/scripts/services/firebase.js
+++ app/scripts/services/firebase.js
@@ -1,22 +1,29 @@
-'use strict';
+// 'use strict';
 
-EconomicsApp.factory('firebase', function() {
-	// Service logic
-	// ...
-	var db = new Firebase('https://creative.firebaseio.com/stp_okonomi');
+// EconomicsApp.factory('firebase', function($rootScope)
+// {
+// 	// Service logic
+// 	// ...
 
-	var dbdata;
+	
 
-	// Public API here
-	return {
-    	get: function()
-    	{
-    		return db.once('value', function(data)
-    		{
-        		dbdata = data.val();
-    		});
-      	
-      		return dbdata;
-    	}
-  	};
-});
+// 	var db = new Firebase('https://creative.firebaseio.com/stp_okonomi');
+
+// 	var send = function(dbdata)
+// 	{
+// 		lines = dbdata;
+// 		$rootScope.$apply();
+// 	}
+	
+// 	var get = function()
+// 	{
+// 		db.once('value', function(data)
+// 		{
+// 			send(_.toArray(data.val()).slice(1));
+// 		});
+// 	}
+
+// 	return {
+//     	$get: get
+//   	};
+// });
