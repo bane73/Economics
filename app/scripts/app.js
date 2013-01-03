d3aaa11ed060a4d34a7a807d5ea4c2e70b1302bc
Kenneth Lynne <kenneth.lynne@gmail.com>
2013-01-03 12:11:22 +0100

    Yeoman init angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var EconomicsApp = angular.module('EconomicsApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+EconomicsApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
